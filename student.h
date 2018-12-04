0dee3ac88197056f08aafc102f691b98de5745ce
drewcornatzer <acornatzer@mail.csuchico.edu>
2018-12-03 16:21:28 -0800

    Beginning of file

---
+++ student.h
@@ -0,0 +1,23 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+#include <iostream>
+#include <string>
+
+class student
+{
+        private:
+        std::string name1;
+        std::string name2;
+
+        public:
+        void setName(std::string,std::string)
+        {
+
+        }
+        std::string fullName()
+        {
+
+        }
+};
+
+#endif
